472aa775fbea60d671cb819e9b3d3dfc6b7f922c
Allisson100 <allisson.boiane@outlook.com>
2024-08-31 15:24:43 -0300

    unip: projetoDeExtensao

--- lib/backend/firebase/firebase_config.dart
+++ lib/backend/firebase/firebase_config.dart
@@ -5,13 +5,13 @@ Future initFirebase() async {
   if (kIsWeb) {
     await Firebase.initializeApp(
         options: FirebaseOptions(
-            apiKey: "AIzaSyCrQzdIr2N349eNhH1gzJwC4BCrzuvKT10",
-            authDomain: "caminho-de-volta.firebaseapp.com",
-            projectId: "caminho-de-volta",
-            storageBucket: "caminho-de-volta.appspot.com",
-            messagingSenderId: "97586645382",
-            appId: "1:97586645382:web:f420934ac1edfecc2cc407",
-            measurementId: "G-1TV07S5097"));
+            apiKey: "",
+            authDomain: "",
+            projectId: "",
+            storageBucket: "",
+            messagingSenderId: "",
+            appId: "",
+            measurementId: ""));
   } else {
     await Firebase.initializeApp();
   }
